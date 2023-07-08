987441782ed9b8d5e4229afd9a2a11cd0ca7a5a6
BlancAlexis <alexis.blanc@etu.univ-lyon1.fr>
2023-07-09 01:14:53 +0200

    add data send to the wear

--- mobile/src/main/java/fr/alexis/getphonebatterystatus/BatteryListenerService.kt
+++ mobile/src/main/java/fr/alexis/getphonebatterystatus/BatteryListenerService.kt
@@ -1,32 +1,31 @@
 package fr.alexis.getphonebatterystatus
 
+import android.util.Log
 import com.google.android.gms.wearable.*
+import fr.alexis.getphonebatterystatus.BatteryStatus.batteryCharging
+import fr.alexis.getphonebatterystatus.BatteryStatus.batteryLevel
 
 class BatteryListenerService : WearableListenerService(), BatteryUpdateInterface {
-    override fun onDataChanged(dataEvents: DataEventBuffer) {
-        for (event in dataEvents) {
-            if (event.type == DataEvent.TYPE_CHANGED && event.dataItem.uri.path == "/battery_data") {
-                val dataMap = DataMapItem.fromDataItem(event.dataItem).dataMap
-                val batteryLevel = dataMap.getInt("battery_level", 0)
-                val batteryCharging = dataMap.getInt("battery_charging", -1)
-                // Mettez à jour l'interface utilisateur avec les données de la batterie
-            }
+    override fun batteryDataChanged() {
+        sendBatteryDataToWear()
+    }
+
+    fun sendBatteryDataToWear() {
+        val dataMap = DataMap().apply {
+            putInt("battery_level", BatteryStatus.batteryLevel)
+            putInt("battery_charging", BatteryStatus.batteryCharging)
+        }
+        val dataItem = PutDataMapRequest.create("/battery_data").apply {
+            dataMap.putAll(dataMap)
+        }.asPutDataRequest()
+
+        val dataClient = Wearable.getDataClient(applicationContext)
+        val putDataTask = dataClient.putDataItem(dataItem)
+        putDataTask.addOnSuccessListener {
+            Log.i("Succès", "Envoye des données batterie = $batteryLevel ainsi qu'elle charge = $batteryCharging ")
+        }
+        putDataTask.addOnFailureListener { exception ->
+            Log.i("Echec","Erreur dans l'envoie des donnéées $exception")
         }
     }
-
-    override fun batteryDataChanged() {
-        onDataChanged()
-    }
-    val dataMapToSend = DataMap().apply {
-        putInt("battery_level", BatteryStatus.batteryLevel)
-        putInt("battery_charging", BatteryStatus.batteryCharging)
-    }
-
-    val dataItem = PutDataMapRequest.create("/battery_data").apply {
-        dataMap = this.dataMapToSend
-    }.asPutDataRequest()
-
-    val dataClient = Wearable.getDataClient(context!!)
-    dataClient.putDataItem(dataItem)
-}
 }
