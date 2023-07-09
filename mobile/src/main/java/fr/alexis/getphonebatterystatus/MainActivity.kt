85b0b2ffaa3560c9ffa901163f2260e5a6cd00d1
BlancAlexis <alexis.blanc@etu.univ-lyon1.fr>
2023-07-09 12:16:02 +0200

    récup info batterie ok

--- mobile/src/main/java/fr/alexis/getphonebatterystatus/MainActivity.kt
+++ mobile/src/main/java/fr/alexis/getphonebatterystatus/MainActivity.kt
@@ -7,8 +7,20 @@ import android.os.BatteryManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.android.gms.wearable.MessageClient
+import fr.alexis.getphonebatterystatus.BatteryStatus.batteryCharging
+import fr.alexis.getphonebatterystatus.BatteryStatus.batteryLevel
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),BatteryUpdateInterface {
+    override fun onStart() {
+        super.onStart()
+ val bat = BatteryReceiver(this)
+        val filter = IntentFilter(Intent.ACTION_BATTERY_CHANGED)
+        registerReceiver(bat, filter)
 
+    }
+
+    override fun batteryDataChanged() {
+        println("$batteryLevel $batteryCharging")
+    }
 }
 
